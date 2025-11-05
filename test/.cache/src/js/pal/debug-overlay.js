60d842127a3bac5315ed0ab27008c14a27daa790
jianmingliu <jianmingliu@tencent.com>
2025-11-05 15:21:07 -0800

    More debugUtils src/js/pal/debug-overlay.js: added a browser overlay panel that shows FPS, scene/battle info, and sprite/RLE cache stats with simple start/stop/toggle controls. src/js/pal/debug-utils.js: extended the debug helpers with reactive tracing, render profiling, and new getSpriteStats / getRLEStats methods; wired in the overlay module. src/js/pal/rle.js: now collects RLE decode statistics (call counts, reuse, buffer size histogram) and exports RLE_STATS. src/js/pal/sprite.js: tracks sprite creation, frame counts, buffer size, and exports SPRITE_STATS.

---
+++ test/.cache/src/js/pal/debug-overlay.js
@@ -0,0 +1,9 @@
+const overlayStub = {
+  startOverlay: () => {},
+  stopOverlay: () => {},
+  toggleOverlay: () => {}
+};
+export const startOverlay = overlayStub.startOverlay;
+export const stopOverlay = overlayStub.stopOverlay;
+export const toggleOverlay = overlayStub.toggleOverlay;
+export default overlayStub;
--- test/setup.js
+++ test/setup.js
@@ -1,67 +1,27 @@
-const noop = () => {};
-
-globalThis.PAL_CLASSIC = true;
-
-globalThis.Global = globalThis.Global || {};
-globalThis.GameData = globalThis.GameData || {};
-globalThis.Files = globalThis.Files || {};
-globalThis.DEBUG = globalThis.DEBUG || { Timing: false };
-
-globalThis.log = globalThis.log || {
-  trace: noop,
-  debug: noop,
-  info: noop,
-  warning: noop,
-  error: noop,
-  fatal: noop
-};
-
-globalThis.sleep = () => Promise.resolve();
-globalThis.sleepByFrame = () => Promise.resolve();
-
-globalThis.memset = (array, value, length) => {
-  if (!array) return array;
-  const len = length != null ? length : array.length;
-  for (let i = 0; i < len; i++) {
-    array[i] = value;
-  }
-  return array;
-};
-
-globalThis.memcpy = (target, source, length) => {
-  if (!target || !source) return target;
-  const len = length != null ? length : Math.min(target.length, source.length);
-  for (let i = 0; i < len; i++) {
-    target[i] = source[i];
-  }
-  return target;
-};
-
-globalThis.PAL_XY = (x, y) => ((x & 0xFFFF) << 16) | (y & 0xFFFF);
-globalThis.timestamp = () => Date.now();
-
-
-if (!globalThis.crypto || typeof globalThis.crypto.getRandomValues !== 'function') {
-  const { webcrypto, randomBytes } = await import('crypto');
-  if (webcrypto && typeof webcrypto.getRandomValues === 'function') {
-    globalThis.crypto = webcrypto;
-  } else {
-    globalThis.crypto = {
-      getRandomValues(typedArray) {
-        if (!typedArray || typeof typedArray.length !== 'number' || !ArrayBuffer.isView(typedArray)) {
-          throw new TypeError('Expected typed array view');
-        }
-        const bytes = randomBytes(typedArray.byteLength);
-        const TypedArrayConstructor = typedArray.constructor;
-        const temp = new TypedArrayConstructor(bytes.buffer, bytes.byteOffset, typedArray.length);
-        typedArray.set(temp);
-        return typedArray;
-      }
-    };
-  }
+if (typeof globalThis.PAL_CLASSIC === 'undefined') {
+  globalThis.PAL_CLASSIC = false;
+}
+if (!globalThis.GameData) {
+  globalThis.GameData = {};
+}
+if (!globalThis.Global) {
+  globalThis.Global = {};
+}
+if (!globalThis.log) {
+  const noop = () => {};
+  globalThis.log = {
+    trace: noop,
+    debug: noop,
+    info: noop,
+    warn: noop,
+    error: noop
+  };
+}
+if (typeof globalThis.sprintf !== 'function') {
+  globalThis.sprintf = (...args) => args.join(' ');
 }
 
-globalThis.FrameTime = globalThis.FrameTime || (1000 / 24);
-
+await import('../test/.cache/src/js/pal/debug-overlay.js');
 await import('../src/js/pal/binary-helper.js');
 await import('../src/js/pal/pal-global.js');
+await import('../src/js/pal/common.js');
